5e0b801aa691e7d5db1207ff0fdcba6736e06331
lucasikruger <lucaskruger10@gmail.com>
2023-08-07 15:49:01 -0300

    bug argument use_refiner solved

--- app/src/functions.py
+++ app/src/functions.py
@@ -3,4 +3,4 @@ from src.stable_diffusion_xl import StableDifussionXL
 
 st.cache_resource
 def load_model(use_refiner, mem_offload):
-    return StableDifussionXL(use_refiner=use_refiner, mem_offload=mem_offload)
+    return StableDifussionXL(use_refiner, mem_offload)
