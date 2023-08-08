126d3841372b72ea389683462487f432d227fe25
lucasikruger <lucaskruger10@gmail.com>
2023-08-08 15:12:16 -0300

    improbed app/app.py

--- app/app.py
+++ app/app.py
@@ -29,20 +29,47 @@ else:
 prompt = st.text_input("Prompt", value= "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k", key="prompt")
 negative_prompt= st.text_input("Negative Prompt", value="", key="negative_prompt")
 
-if st.button("Generate"):
-    with st.spinner("Generating..."):
-        st.text (str(prompt))
-        model = load_model(use_refiner=use_refiner, mem_offload=mem_offload)
-        seed = None
-        images = model.infer(
-            prompt=str(prompt),
-            negative_prompt=str(negative_prompt),
-            num_images_per_prompt=int(num_images_per_prompt),
-            seed=seed,
-            n_steps=int(n_steps),
-            use_refiner=use_refiner,
-            high_noise_frac=high_noise_frac,
+# At the beginning of your script, initialize session states:
+if 'inference_ongoing' not in st.session_state:
+    st.session_state.inference_ongoing = False
+if 'images' not in st.session_state:
+    st.session_state.images = []
+    
+# Button which checks session state:
+if st.button("Generate") and not st.session_state.inference_ongoing:
+        
+    # Set the state to inference ongoing
+    st.session_state.inference_ongoing = True
+    st.warning("Inference is ongoing. Please wait...")
+    # Inference
+    try:
+        with st.spinner("Loading Model..."):
+            model = load_model(use_refiner=use_refiner, mem_offload=mem_offload)
+        with st.spinner("Generating..."):
+            images = model.infer(
+                prompt=str(prompt),
+                negative_prompt=str(negative_prompt),
+                num_images_per_prompt=int(num_images_per_prompt),
+                seed=seed,
+                n_steps=int(n_steps),
+                use_refiner=use_refiner,
+                high_noise_frac=high_noise_frac,
             )
+        
+        st.session_state.images = images
+        
+        # Save images
         for image in images:
-            st.image(image, width=512)
             image.save(f"output/{prompt}-{negative_prompt}-{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.png")
+    
+        # Reset the inference ongoing state and rerun to display results
+        st.session_state.inference_ongoing = False
+        st.experimental_rerun()
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+        st.session_state.inference_ongoing = False  # Ensure the session state is reset even if an error occurs
+
+    
+for image in st.session_state.images:
+    st.image(image, width=512)
+
