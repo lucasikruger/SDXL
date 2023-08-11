6733f838c5d713f7233ce6f09f46db637c7b859b
lucasikruger <lucaskruger10@gmail.com>
2023-08-11 18:29:22 -0300

    changes in api_server: added comparer, added comparer_mock, upgraded requirements, modified api_server.py

--- api+client/api_server/app/src/api_server.py
+++ api+client/api_server/app/src/api_server.py
@@ -4,11 +4,26 @@ from PIL import Image
 import base64
 import io
 from src.model_mock import StableDifussionXL
+from src.image_comparer_mock import ImageComparer
 from threading import Lock
-
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+# Allow connections from anywhere
+origins = ["*"]
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 model_lock = Lock()
+comparer_lock = Lock()
 
 # Initialize the model
 model = StableDifussionXL(refiner=False, mem_offload=False)
@@ -46,3 +61,25 @@ async def generate_image(prompt: ImagePrompt):
         base64_images.append(img_base64)
     
     return {"images": base64_images, "prompt": prompt.prompt, "negative_prompt": prompt.negative_prompt, "seed": prompt.seed, "n_steps": prompt.n_steps, "use_refiner": prompt.use_refiner, "high_noise_frac": prompt.high_noise_frac}
+
+
+comparer = ImageComparer()
+
+class Images(BaseModel):
+    image1: str
+    image2: str
+
+def decode_image(image_base64: str) -> Image:
+    image_bytes = base64.b64decode(image_base64)
+    image = Image.open(io.BytesIO(image_bytes))
+    return image
+
+@app.post("/compare")
+async def compare_api(images: Images):
+    with comparer_lock:
+        image1 = decode_image(images.image1).convert('RGB')
+        image2 = decode_image(images.image2).convert('RGB')
+        
+        ret = comparer.compare(image1, image2)
+
+        return ret
---
+++ api+client/api_server/app/src/image_comparer.py
@@ -0,0 +1,38 @@
+from huggingface_hub import from_pretrained_keras
+from PIL import Image
+import numpy as np
+from skimage.metrics import structural_similarity as ssim
+import torch
+import clip
+
+class ImageComparer():
+    def __init__(self) -> None:
+        
+        self._device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self._model_cifar = from_pretrained_keras("keras-io/cifar10_metric_learning")
+        self._model, self._preprocess = clip.load("ViT-L/14", device=self._device)
+
+    def compare(self, image1, image2):
+        # Change images to rgb or bgr
+        img1 = self._preprocess(image1).to(self._device)
+        img2 = self._preprocess(image2).to(self._device)
+        print(img1.shape, img2.shape)
+        img = torch.stack([img1, img2])
+        with torch.no_grad():
+            image_features = self._model.encode_image(img)
+        clip_score = image_features.corrcoef()[0, 1].item()
+
+        img1 = np.array(image1.resize((32, 32)))[:,:,:3]
+        img2 = np.array(image2.resize((32, 32)))[:,:,:3]
+        img1 = np.expand_dims(img1 / 255, axis=0)
+        img2 = np.expand_dims(img2 / 255, axis=0)
+        print(img1.shape, img2.shape)
+        img = np.append(img1, img2, axis=0)
+        embeddings = self._model_cifar.predict(img)
+        cifar_score = np.corrcoef(embeddings)[0, 1]
+
+        ssim_score = ssim(np.array(image1), np.array(image2), channel_axis=2)
+
+        mean_score = (clip_score + ssim_score + cifar_score) / 3
+        ret = {"clip": clip_score, "ssim": ssim_score, "cifar": cifar_score, "mean": mean_score}
+        return ret
